d0d35532c7a9803d9b26e0ddc230a9448eaa2d63
Hengly Aun <aunhengly@gmail.com>
2022-05-23 17:30:50 -0700

    first committ

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,43 +1,47 @@
 import React, { Component} from 'react';
+import { connect } from 'react-redux'
 import './App.css'
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import Header from '../components/Header';
 import ErrorBoundry from '../components/ErrorBoundry';
 
+import { requestRobots, setSearchField } from  '../actions'
+
+const mapStateToProps = state => {
+   return {
+      searchField: state.searchRobots.searchField,
+      robots: state.requestRobots.robots,
+      isPending: state.requestRobots.isPending,
+      error: state.requestRobots.error
+   }
+}
+
+const mapDispatchToProps = (dispatch) => {
+   return {
+      onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+      onRequestRobots: () => dispatch(requestRobots())
+   }
+}
+
 class App extends Component {
-   constructor(){
-      super()
-      this.state = {
-         robots: [],
-         searchfield:''      
-      }
-   }
-
-   componentDidMount(){
-      fetch('https://jsonplaceholder.cypress.io/users')
-         .then(response => response.json())
-         // .then(users => {});
-         .then(users => this.setState({robots: users}));
-
-   }
-
-   onSearchChange = (event) => {
-      this.setState({searchfield:event.target.value})
+   componentDidMount() {
+      this.props.onRequestRobots();
    }
    
    render() {
-      const {robots, searchfield} = this.state;
+      const {searchField, onSearchChange,robots, isPending} = this.props;
       const filteredRobots = robots.filter(robots => {
-         return robots.name.toLowerCase().includes(searchfield.toLowerCase());
+         return robots.name.toLowerCase().includes(searchField.toLowerCase());
       });
       
-      return !robots.length ?
+      return isPending ?
          <h1>Loading</h1> :
          (
             <div className="tc">
-               <h1 className="f1">Robot Friend</h1>
-               <SearchBox searchChange={this.onSearchChange}/>
+               <Header />
+               <SearchBox searchChange={ onSearchChange }/>
                 <Scroll>
                    <ErrorBoundry>
                      <CardList robots= {filteredRobots}/>
@@ -48,4 +52,4 @@ class App extends Component {
    }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -4,17 +4,25 @@ import './index.css';
 import App from './containers/App';
 
 import 'tachyons';
-
 import reportWebVitals from './reportWebVitals';
+
+import { Provider, connect } from 'react-redux';
+import { createStore, applyMiddleware,combineReducers } from 'redux';
+import { createLogger } from 'redux-logger'
+import { searchRobots, requestRobots } from './reducer';
+import thunkMiddleware from 'redux-thunk';
+
+const logger = createLogger();
+const rootReducer = combineReducers({ searchRobots, requestRobots })
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
