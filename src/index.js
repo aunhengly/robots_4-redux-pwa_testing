83914b187378d77c958368f7d0db75d005b7a851
Hengly Aun <aunhengly@gmail.com>
2022-05-24 17:32:15 -0700

    looking for why pwa not full successful

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './containers/App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import * as serviceWorker from './serviceWorker';
 
 import 'tachyons';
 import reportWebVitals from './reportWebVitals';
@@ -29,6 +30,9 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+
+// serviceWorkerRegistration.register();
+serviceWorker.register();
+
 
 reportWebVitals();
